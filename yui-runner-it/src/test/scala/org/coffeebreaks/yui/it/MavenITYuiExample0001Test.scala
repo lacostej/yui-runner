ede430bef97e351dcaed7f50524b46e3e68dc80c
Jerome Lacoste <jerome.lacoste@gmail.com>
2011-01-04 12:15:52 +0100

    Convert the example into an integration test, ran with the maven-verifier

---
+++ yui-runner-it/src/test/scala/org/coffeebreaks/yui/it/MavenITYuiExample0001Test.scala
@@ -0,0 +1,30 @@
+package org.coffeebreaks.yui.it
+
+import org.junit.Test
+import org.junit.Assert._
+import java.io.File
+import org.apache.maven.it.Verifier
+import org.apache.maven.it.util.ResourceExtractor
+
+class MavenITYuiExample0001Test {
+  @Test def testSurefireWrapping() = {
+    val testDir: File = ResourceExtractor.simpleExtractResources( getClass(), "/example-0001" )
+    val verifier: Verifier = new Verifier(testDir.getAbsolutePath)
+
+    verifier.displayStreamBuffers
+
+    /*val cliOptions: List = [ "-N" ]
+    verifier.setCliOptions()
+    */
+    verifier.executeGoal("install")
+
+    verifier.verifyTextInLog("Tests run: 3, Failures: 0, Errors: 2, Skipped: 1, Time elapsed:")
+    verifier.verifyTextInLog("Tests in error:")
+    verifier.verifyTextInLog("example2Test.html(myapp.YUI2Test)")
+    verifier.verifyTextInLog("example1Test.html(myapp.YUI2Test)")
+
+    verifier.verifyErrorFreeLog
+
+    verifier.resetStreams
+  }
+}
