374b84546f7299ec9d39eec7ba4b0290ce72c2c6
Jerome Lacoste <jerome.lacoste@gmail.com>
2011-01-05 13:09:17 +0100

    Add some dummy Java classes to generate some sort of Javadoc to please the OSS Sonatype staging promotion validation procedure...

---
+++ yui-collector/src/main/scala/org/coffeebreaks/yui/dummy/DummyClass.java
@@ -0,0 +1,9 @@
+package org.coffeebreaks.yui.dummy;
+
+/**
+ * This class only serves to please the requirements of the publication of artifacts onto OSS Sonatype repository
+ * @author Jerome Lacoste
+ */
+public class DummyClass {
+
+}
---
+++ yui-collector/src/main/scala/org/coffeebreaks/yui/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the classes necessary to run and report YUI tests
+ * @since 0.1
+ */
+ package org.coffeebreaks.yui;
