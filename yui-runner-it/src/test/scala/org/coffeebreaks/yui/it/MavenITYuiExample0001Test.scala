798097ec4497fe9e141b5d9fa3bffc258fd8dd12
Jerome Lacoste <jerome.lacoste@gmail.com>
2011-01-06 12:47:11 +0100

    Update integration tests with new runner implementation. Not all html files are picked, thus the number of tests reported is different

--- yui-runner-it/src/test/scala/org/coffeebreaks/yui/it/MavenITYuiExample0001Test.scala
+++ yui-runner-it/src/test/scala/org/coffeebreaks/yui/it/MavenITYuiExample0001Test.scala
@@ -5,26 +5,38 @@ import org.junit.Assert._
 import java.io.File
 import org.apache.maven.it.Verifier
 import org.apache.maven.it.util.ResourceExtractor
+import scala.collection.JavaConversions._
 
 class MavenITYuiExample0001Test {
   @Test def testSurefireWrapping() = {
     val testDir: File = ResourceExtractor.simpleExtractResources( getClass(), "/example-0001" )
     val verifier: Verifier = new Verifier(testDir.getAbsolutePath, null, true)
     
-    verifier.displayStreamBuffers
+    //verifier.setMavenDebug(true)
 
     /*val cliOptions: List = [ "-N" ]
     verifier.setCliOptions()
     */
     verifier.executeGoal("install")
 
-    verifier.verifyTextInLog("Tests run: 3, Failures: 0, Errors: 2, Skipped: 1, Time elapsed:")
+    verifier.displayStreamBuffers
+
+    VerifierUtil.displayMavenLog(verifier)
+
+    verifier.verifyTextInLog("Tests run: 2, Failures: 0, Errors: 2, Skipped: 0, Time elapsed:")
     verifier.verifyTextInLog("Tests in error:")
-    verifier.verifyTextInLog("example2Test.html(myapp.YUI2Test)")
     verifier.verifyTextInLog("example1Test.html(myapp.YUI2Test)")
+    verifier.verifyTextInLog("example2Test.html(myapp.YUI2Test)")
 
     verifier.verifyErrorFreeLog
 
     verifier.resetStreams
+  }
+}
+
+object VerifierUtil {
+  def displayMavenLog(verifier: Verifier): Unit = {
+    val lines = verifier.loadFile(verifier.getBasedir(), verifier.getLogFileName(), false)
+    lines.foreach(line => println(line))
   }
 }
